2007288811ccff83f71bda1e507fea7f54b72416
ThomasLi0314 <jl15535@nyu.edu>
2025-12-16 12:48:53 -0500

    Delete Circle_in_Flow_1206 directory

--- Circle_in_Flow_1206/vorticity_cal.m
+++
@@ -1,6 +0,0 @@
-function omega = vorticity_cal(u, ind_fx, ind_bx, ind_fy, ind_by, dx, dy)
-    p_xv = (u(ind_fx, :, 2) - u(ind_bx, :, 2)) / (2 * dx);
-    p_yu = (u(:, ind_fy, 1) - u(:, ind_by, 1)) / (2 * dy);
-
-    omega = p_xv - p_yu;
-end
